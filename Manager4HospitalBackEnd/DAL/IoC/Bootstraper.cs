fc4708f0116fd658158098d7c3cd90356ac5386f
moransaka19 <moransaka19@gmail.com>
2020-09-11 08:32:51 +0300

    add IoC

---
+++ Manager4HospitalBackEnd/DAL/IoC/Bootstraper.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace DAL.IoC
+{
+    public static class Bootstraper
+    {
+        public static void Bootstrap(ContainerBuilder container)
+        {
+            container.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.Name.EndsWith("Repository") && !x.IsAbstract)
+                .AsImplementedInterfaces()
+                .WithParameter(
+                    (paramInfo, _) => paramInfo.Name == "connectionString",
+                    (_, contex) => contex.Resolve<IConfiguration>().GetConnectionString("DefaultConnection")
+                );
+        }
+    }
+}
--- Manager4HospitalBackEnd/Manager4HospitalBackEnd/Startup.cs
+++ Manager4HospitalBackEnd/Manager4HospitalBackEnd/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,11 @@ namespace Manager4HospitalBackEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            DAL.IoC.Bootstraper.Bootstrap(builder);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
